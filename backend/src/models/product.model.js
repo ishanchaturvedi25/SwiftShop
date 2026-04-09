ab10ef28717b93b73789085e97a30052df9fb068
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2026-04-09 23:26:03 +0530

    Add Mongoose models for Cart, Order, Product, and User

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: 'String',
+    description: 'String',
+    price: 'Number',
+    category: 'String',
+    imageUrl: 'String',
+    stock: 'Number',
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);

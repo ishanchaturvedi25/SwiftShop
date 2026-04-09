93ba31a1223d617045208bb3f764e1658ce5f86f
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2026-04-09 22:52:50 +0530

    Initialize backend with Express server and dependencies

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to SwiftShop API!');
+});
+
+module.exports = app;

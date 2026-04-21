83f889df0dbd8d6c1266d9cecfc01afe9f995419
ishanchaturvedi25 <ishanchaturvedi25@gmail.com>
2026-04-21 23:07:38 +0530

    Add footer and new components for policy and newsletter subscription. Update assets with new icons and images

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,6 +4,9 @@ import cart_icon from './cart_icon.png'
 import profile_icon from './profile_icon.png'
 import search_icon from './search_icon.png'
 import menu_icon from './menu_icon.png'
+import exchange_icon from './exchange_icon.png'
+import quality_icon from './quality_icon.png'
+import support_img from './support_img.png'
 
 export const assets = {
     logo,
@@ -12,4 +15,7 @@ export const assets = {
     profile_icon,
     search_icon,
     menu_icon,
+    exchange_icon,
+    quality_icon,
+    support_img,
 }
